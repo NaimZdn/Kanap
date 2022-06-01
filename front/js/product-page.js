515d018d5f76248b098566bd1c8ac580469f6a43
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-01 14:59:13 +0200

    refractorindg

--- front/js/product-page.js
+++ front/js/product-page.js
@@ -79,42 +79,34 @@ function addToCartProduct (){
                 color: `${colorProduct}`, 
                 quantity : `${quantityProduct}`,
                 id : `${productId}`,
-            
             })
-
+    // Si le localStorage est vide alors on push les infos du produit que l'on souhaite ajouter au panier 
         if (productBoard == null ) {
             productBoard = []; 
             productBoard.push(productInfo); 
             localStorage.setItem('productInCart', JSON.stringify(productBoard));
     
-        } else if (productBoard != null) {
-            for (i = 0; i < productBoard.length; i++) {
-                if (productBoard[i].id == productId && productBoard[i].color == colorProduct ){
-                        let quantityValue = parseInt(productBoard[i].quantity) + parseInt(quantityProduct)
-                                      
-                return (productBoard[i].quantity = quantityValue, 
-                    localStorage.setItem("productInCart",JSON.stringify(productBoard)), 
-                    (productBoard = JSON.parse(localStorage.getItem("productBoard"))))
-                }         
-            }
-            for (i = 0; i < productBoard.length; i++) {
-                if ((productBoard[i].id == productId && productBoard[i].color != colorProduct) || productBoard[i].id != productId);
-             return (
-                    productBoard.push(productInfo),
-                    localStorage.setItem("productInCart", JSON.stringify(productBoard)),
-                    (productBoard = JSON.parse(localStorage.getItem("productBoard")))
-            )};
-        
-    }});
-    return (productBoard = JSON.parse(localStorage.getItem("productInCart")));   
+        // Sinon, si il n'est pas vide alors on peut executer une de ces 2 conditions 
+            } else if (productBoard != null) {
+
+            // Si le produit dans le localStorage est le même que le produit que l'on souhaite ajouté alors on modifie seulement la quantité du produit
+                for (i = 0; i < productBoard.length; i++) {
+                    if (productBoard[i].id == productId && productBoard[i].color == colorProduct ){
+                            let quantityValue = parseInt(productBoard[i].quantity) + parseInt(quantityProduct)
+                                        
+                    return (productBoard[i].quantity = quantityValue, 
+                        localStorage.setItem("productInCart",JSON.stringify(productBoard)))
+                    }         
+                }
+            // Si le produit est le même mais sa couleur est différente alors on ajoute un nouveau produit, même procédé si le produit est différent
+                for (i = 0; i < productBoard.length; i++) {
+                    if ((productBoard[i].id == productId && productBoard[i].color != colorProduct) || productBoard[i].id != productId);
+
+                    return (productBoard.push(productInfo),
+                            localStorage.setItem("productInCart", JSON.stringify(productBoard)))
+                        };
+        }});
     }
-
-
-
-
-
-
-
 
 function orderValidIf(colorProduct, quantityProduct) {
     if (colorProduct == null || colorProduct === "" || quantityProduct == null || quantityProduct == 0 ) {
